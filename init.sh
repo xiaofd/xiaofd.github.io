8fea83603a22f8eb2055ef11332c4530553db742
xiaofd <568032134@qq.com>
2023-12-30 06:17:38 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -47,6 +47,11 @@ else
   elif [[ -n `cat /etc/issue | grep "Debian"` ]] ; then
     sed -i '/^alias ll=*/d' ~/.bashrc
     echo "alias ll='ls -l --color=auto'" >>~/.bashrc
+    
+    apt install fonts-wqy-zenhei
+    echo "zh_CN.UTF-8 UTF-8" > /etc/locale.gen
+    locale-gen
+    update-locale LANG=zh_CN.UTF-8
   fi
 
 
